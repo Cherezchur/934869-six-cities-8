61a87112cfb612aa31f6dcc563c3368f1d2c4fa4
Cherezchur <49197652+Cherezchur@users.noreply.github.com>
2021-10-06 20:05:46 +0300

    Добавлены компоненты для всех страниц проекта

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,19 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../main/main';
+// import SingIn from '../sing-in/sing-in';
+// import Favorites from '../favorites/favorites';
+// import Room from '../room/room';
+
+type AppProps = {
+  rentalOffersCount: number;
+}
+
+function App({rentalOffersCount}: AppProps): JSX.Element {
+  return (
+    <Main rentalOffersCount={rentalOffersCount}/>
+    // <SingIn />
+    // <Favorites />
+    // <Room />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  RENTAL_OFFERS_COUNT: 313,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      rentalOffersCount = {Setting.RENTAL_OFFERS_COUNT}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
